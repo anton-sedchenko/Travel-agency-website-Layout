88fa5b1565a9353d27d3515de2aa5d04aeef0f59
Anton Sedchenko <anton.sedchenko@gmail.com>
2025-04-24 12:54:47 +0300

    animation and sticky header added

--- js/script.js
+++ js/script.js
@@ -1,10 +1,22 @@
 'use strict';
 
 const links = document.querySelectorAll('.nav__list__item');
+const header = document.querySelector('.header');
+const navLogo = document.querySelector('.nav__logo');
 
 links.forEach(link => {
     link.addEventListener('click', (e) => {
         links.forEach(l => l.classList.remove('nav__list__item-active'));
         link.classList.add('nav__list__item-active');
     });
+});
+
+window.addEventListener('scroll', function () {
+    if (window.scrollY > 75) {
+        header.classList.add('header-sticky');
+        navLogo.src = 'img/logo-2.svg';
+    } else {
+        header.classList.remove('header-sticky');
+        navLogo.src = 'img/logo.svg';
+    }
 });
